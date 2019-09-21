7e19e2c45709bf3eae2834e5c8ccecd829ae1913
jothadete <jens.tenter@live.com>
2019-09-21 23:12:00 +0200

    init

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root to: 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'linklist', to: 'pages#linklist'
+  resources :guides
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
