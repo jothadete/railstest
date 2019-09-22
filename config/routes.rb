5a2c837bd150889312b52d893fd21e010b9bd038
jothadete <jens.tenter@live.com>
2019-09-22 22:38:27 +0200

    added Route to vuetest01

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'home', to: 'pages#home'
   get 'linklist', to: 'pages#linklist'
+  get 'vuetest01', to: 'pages#vuetest01'
   resources :guides
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
